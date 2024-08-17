d5d494c493fd2070ce5940fd63356ed3aab78dff
DemArtem <demar.demidov@yandex.ru>
2024-08-17 16:20:29 +0300

    test

---
+++ src/test/java/com/ivl_plus/blog/BlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivl_plus.blog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
