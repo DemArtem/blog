5b15c8dfa728e2896cc1d768b29efe4158f0f609
gk <demar.demidov@yandex.ru>
2024-10-07 21:25:00 +0300

    test

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO product(id,title, description,price) VALUES (1,"Доска обрезная", "1 куб",300);
-INSERT INTO product(id,title, description,price) VALUES (2,"Доска обрезная сухая", "1 куб",500);
+INSERT INTO product(id,title, description, price) VALUES (1,"Доска обрезная", "1 м.куб.", 300);
+INSERT INTO product(id,title, description, price) VALUES (2,"Доска обрезная сухая", "1 м.куб.", 500);
