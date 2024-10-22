474f20c028d3cdfabfe5489c01fbf49014ca4a23
DemArtem <demar.demidov@yandex.ru>
2024-10-22 18:35:25 +0300

    test

--- src/main/java/com/ivl_plus/blog/controllers/MainController.java
+++ src/main/java/com/ivl_plus/blog/controllers/MainController.java
@@ -2,19 +2,40 @@ package com.ivl_plus.blog.controllers;
 
 import com.ivl_plus.blog.models.Category;
 import com.ivl_plus.blog.models.Product;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
 @Controller
+//@RestController
+//@RequestMapping("/api/v1")
+//@RequiredArgsConstructor
 public class MainController {
 
 	@GetMapping("/")
 	public String home(Model model) {
+		List<Product> products = new ArrayList<>();
+
+		// Добавляем продукты в список !!!!!
+		products.add(new Product("Доска обрезная", "img/1.обрез.jpg", BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+
+
+		// Добавляем список продуктов в модель
+		model.addAttribute("products", products);
 		model.addAttribute("title", "Главная страница");
 		return "home";
 	}
@@ -31,6 +52,8 @@ public class MainController {
 
 		// Добавляем продукты в список !!!!!
 		products.add(new Product("Доска обрезная", "img/1.обрез.jpg", BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
 		products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
 
 
--- src/main/java/com/ivl_plus/blog/controllers/ProductController.java
+++ src/main/java/com/ivl_plus/blog/controllers/ProductController.java
@@ -4,6 +4,7 @@ import com.ivl_plus.blog.models.Category;
 import com.ivl_plus.blog.models.Product;
 import com.ivl_plus.blog.services.ProductService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,26 +15,28 @@ import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
-@RestController
-@RequestMapping("/api/v1")
-@RequiredArgsConstructor
-public class ProductController {
-    private final ProductService productService;
-
-    @GetMapping("/products")
-    public String getProducts(Model model) {
-        List<Product> products = new ArrayList<>();
-
-        // Добавляем продукты в список !!!!!
-        products.add(new Product("Доска обрезная", "img/1.обрез.jpg", BigDecimal.valueOf(300),new Category(1L,"Доска")));
-        products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
-
-
-        // Добавляем список продуктов в модель
-        model.addAttribute("products", products);
-        return "products"; // Имя шаблона
-    }
-}
+//@Controller
+//@RestController
+//@RequestMapping("/api/v1")
+//@RequiredArgsConstructor
+//public class ProductController {
+//    private final ProductService productService;
+//
+//    @GetMapping("/products")
+//    public String getProducts(Model model) {
+//        List<Product> products = new ArrayList<>();
+//
+//        // Добавляем продукты в список !!!!!
+//        products.add(new Product("Доска обрезная", "img/1.обрез.jpg", BigDecimal.valueOf(300),new Category(1L,"Доска")));
+//        products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+//        products.add(new Product("Доска обрезная сухая", "img/2.обрез_сух.jpg",BigDecimal.valueOf(300),new Category(1L,"Доска")));
+//
+//
+//        // Добавляем список продуктов в модель
+//        model.addAttribute("products", products);
+//        return "products"; // Имя шаблона
+//    }
+//}
 
 //подключить авторизацию   https://github.com/KirillovItstep/a0
 //https://github.com/KirillovItstep/a1
