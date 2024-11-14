e665bdaddae27592e11914f49191c37309b2918c
DemArtem <demar.demidov@yandex.ru>
2024-11-14 18:06:23 +0300

    test

--- src/main/java/com/ivl_plus/blog/controllers/SecurityController.java
+++ src/main/java/com/ivl_plus/blog/controllers/SecurityController.java
@@ -5,11 +5,11 @@ import com.ivl_plus.blog.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 public class SecurityController {
+
     @Autowired
     private UserService userService;
 
